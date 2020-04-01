1236e43918f1d974549087b3bf1423708430234b
devFthtnrvr <36139823+devFthtnrvr@users.noreply.github.com>
2020-04-01 12:40:58 +0200

    Add project files.

---
+++ FootballPopup/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FootballPopup
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // it's more typical to pass in parameters as route data
+            // (http://localhost:xxxx/HelloWorld/Welcome?name=Scott&numtimes=4)
+            // http://localhost:xxx/HelloWorld/Welcome/1?name=Scott
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            // You could also add a route to pass both the name and numtimes in parameters as route data in the URL
+            // localhost:XXX/HelloWorld/Welcome/Scott/3
+            routes.MapRoute(
+             name: "Hello",
+             url: "{controller}/{action}/{name}/{id}"
+            );
+        }
+    }
+}
---
+++ FootballPopup/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootballPopup.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+
+    }
+}
